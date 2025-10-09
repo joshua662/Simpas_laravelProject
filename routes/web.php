91f6c5627fbdae1dad0e6ae846d20aac94c9fd16
joshua662 <joshsimpas6.com>
2025-10-09 20:40:41 +0800

    my update update

--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,23 @@
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::get('/dashboard', [StudentController::class, 'index'])->name('dashboard');
+Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
 
-Route::get('/aboutus', function(){
-    return 'This is an about us page';
-});
-
-Route::get('/homePage', function(){
-    return view ('homePage');
-});
-
-Route::get('/contactus', function(){
-    return view ('contactus');
-});
-
-Route::get('/myservice', function(){
-    return view ('myservice');
-});
-
+require __DIR__.'/auth.php';
